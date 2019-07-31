2d27da025f0f478e1fa0cb338d75ad679e1e312e
spaghuette <44427720+spaghuette@users.noreply.github.com>
2019-07-31 19:58:26 +0100

    add audio, randomized backgrounds and button

---
+++ index.js
@@ -0,0 +1,14 @@
+var images = ["mario1", "spaghetti1", "spaghetti2", "spaghetti3", "spaghetti4",
+    "spaghetti5", "spaghetti6"];
+
+function setBackground() {
+    let number = Math.floor(Math.random() * 7);
+    let img = images[number]
+    let uri = `url("images/${img}.png")`
+    document.body.style.backgroundImage = `${uri}`;
+    console.log(uri);
+}
+
+setBackground()
+
+document.onload = document.getElementsByTagName("audio")[0].play()
